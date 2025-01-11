fd738b7d6b46f1afc2c91b231a39904e39b8a2bb
RitickLath <ritick943lath@gmail.com>
2025-01-11 23:28:41 +0530

    Endpoints from ticker

---
+++ Backend/src/APIs/CorporateActions.ts
@@ -0,0 +1,125 @@
+import axios from "axios";
+
+let config = {
+  method: "get",
+  maxBodyLength: Infinity,
+  url: "https://ticker.finology.in/GetCorpAction.ashx?fincode=100325",
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
+    Referer: "https://ticker.finology.in/loader.js?v=2",
+    "x-requested-with": "XMLHttpRequest",
+  },
+};
+
+axios
+  .request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const ResponseSample = {
+  Table: [
+    {
+      "EX Date": "19 Aug 2024",
+      "Record date": "19 Aug 2024",
+      Type: "Final",
+      "Dividend %": 100.0,
+      Amount: 10.0,
+    },
+    {
+      "EX Date": "21 Aug 2023",
+      "Record date": "21 Aug 2023",
+      Type: "Final",
+      "Dividend %": 90.0,
+      Amount: 9.0,
+    },
+    {
+      "EX Date": "18 Aug 2022",
+      "Record date": "19 Aug 2022",
+      Type: "Final",
+      "Dividend %": 80.0,
+      Amount: 8.0,
+    },
+    {
+      "EX Date": "11 Jun 2021",
+      "Record date": "14 Jun 2021",
+      Type: "Final",
+      "Dividend %": 70.0,
+      Amount: 7.0,
+    },
+    {
+      "EX Date": "02 Jul 2020",
+      "Record date": "03 Jul 2020",
+      Type: "Final",
+      "Dividend %": 65.0,
+      Amount: 6.5,
+    },
+  ],
+  Table1: [
+    { "EX Date": "28 Oct 2024", "Record date": "28 Oct 2024", Ratio: "1:1" },
+    { "EX Date": "07 Sep 2017", "Record date": "09 Sep 2017", Ratio: "1:1" },
+  ],
+  Table2: [
+    {
+      "EX Date": "13 May 2020",
+      "Record date": "14 May 2020",
+      Ratio: "1:15",
+      "Offer price": 1257.0,
+    },
+  ],
+  Table3: [],
+  fv: [],
+  bonus: [
+    { ex_date: "28 Oct 2024", Ratio: "1:1", recrd_date: "28 Oct 2024" },
+    { ex_date: "07 Sep 2017", Ratio: "1:1", recrd_date: "09 Sep 2017" },
+  ],
+  rights: [
+    {
+      ex_date: "13 May 2020",
+      Ratio: "1:15",
+      recrd_date: "14 May 2020",
+      Offerprice: 1257.0,
+    },
+  ],
+  Dividend: [
+    {
+      Ex_Date: "19 Aug 2024",
+      Record_Date: "19 Aug 2024",
+      Dividend: 100.0,
+      Type: "Final",
+      Amount: 10.0,
+    },
+    {
+      Ex_Date: "21 Aug 2023",
+      Record_Date: "21 Aug 2023",
+      Dividend: 90.0,
+      Type: "Final",
+      Amount: 9.0,
+    },
+    {
+      Ex_Date: "18 Aug 2022",
+      Record_Date: "19 Aug 2022",
+      Dividend: 80.0,
+      Type: "Final",
+      Amount: 8.0,
+    },
+    {
+      Ex_Date: "11 Jun 2021",
+      Record_Date: "14 Jun 2021",
+      Dividend: 70.0,
+      Type: "Final",
+      Amount: 7.0,
+    },
+    {
+      Ex_Date: "02 Jul 2020",
+      Record_Date: "03 Jul 2020",
+      Dividend: 65.0,
+      Type: "Final",
+      Amount: 6.5,
+    },
+  ],
+};
---
+++ Backend/src/APIs/GroupCompanies.ts
@@ -0,0 +1,137 @@
+import axios from "axios";
+
+let config = {
+  method: "get",
+  maxBodyLength: Infinity,
+  url: "https://ticker.finology.in/GetHouseMaster.ashx?housecode=173",
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
+    Referer: "https://ticker.finology.in/loader.js?v=2",
+    "x-requested-with": "XMLHttpRequest",
+  },
+};
+
+axios
+  .request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const sample = [
+  {
+    FINCODE: 100325,
+    COMPNAME: "Reliance Industries Ltd.",
+    SCRIPCODE: 500325,
+    MCAP: 1680585.3902026203,
+    CLOSE_PRICE: 1241.9,
+    SYMBOL: "RELIANCE",
+    Status: "Active",
+    SNAME: "Reliance Industries",
+    scripcode1: 500325,
+    HOUSE: "Mukesh Ambani",
+    SECTOR: "Refineries",
+    perchange: -1.02,
+  },
+  {
+    FINCODE: 229365,
+    COMPNAME: "JIO Financial Services Ltd.",
+    SCRIPCODE: 543940,
+    MCAP: 178241.38789434,
+    CLOSE_PRICE: 280.55,
+    SYMBOL: "JIOFIN",
+    Status: "Active",
+    SNAME: "JIO Financial Serv.",
+    scripcode1: 543940,
+    HOUSE: "Mukesh Ambani",
+    SECTOR: "Finance - NBFC",
+    perchange: -3.01,
+  },
+  {
+    FINCODE: 132798,
+    COMPNAME: "Network 18 Media & Investments Ltd.",
+    SCRIPCODE: 532798,
+    MCAP: 9711.516113364,
+    CLOSE_PRICE: 62.98,
+    SYMBOL: "NETWORK18",
+    Status: "Active",
+    SNAME: "Network 18 Media Inv",
+    scripcode1: 532798,
+    HOUSE: "Mukesh Ambani",
+    SECTOR: "TV Broadcasting & Software Production",
+    perchange: -5.45,
+  },
+  {
+    FINCODE: 211531,
+    COMPNAME: "Just Dial Ltd.",
+    SCRIPCODE: 535648,
+    MCAP: 8802.51958982,
+    CLOSE_PRICE: 1035.1,
+    SYMBOL: "JUSTDIAL",
+    Status: "Active",
+    SNAME: "Just Dial",
+    scripcode1: 535648,
+    HOUSE: "Mukesh Ambani",
+    SECTOR: "Business Support",
+    perchange: -3.73,
+  },
+  {
+    FINCODE: 215739,
+    COMPNAME: "Hathway Cable & Datacom Ltd.",
+    SCRIPCODE: 533162,
+    MCAP: 2690.5588399999997,
+    CLOSE_PRICE: 15.2,
+    SYMBOL: "HATHWAY",
+    Status: "Active",
+    SNAME: "Hathway Cable & Data",
+    scripcode1: 533162,
+    HOUSE: "Mukesh Ambani",
+    SECTOR: "Telecommunication - Service  Provider",
+    perchange: -3.68,
+  },
+  {
+    FINCODE: 219237,
+    COMPNAME: "Den Networks Ltd.",
+    SCRIPCODE: 533137,
+    MCAP: 1956.6177644999998,
+    CLOSE_PRICE: 41.0,
+    SYMBOL: "DEN",
+    Status: "Active",
+    SNAME: "Den Networks",
+    scripcode1: 533137,
+    HOUSE: "Mukesh Ambani",
+    SECTOR: "TV Broadcasting & Software Production",
+    perchange: -3.14,
+  },
+  {
+    FINCODE: 123445,
+    COMPNAME: "Reliance Industrial Infrastructure Ltd.",
+    SCRIPCODE: 523445,
+    MCAP: 1565.2659999999998,
+    CLOSE_PRICE: 1036.6,
+    SYMBOL: "RIIL",
+    Status: "Active",
+    SNAME: "Reliance Indl. Infra",
+    scripcode1: 523445,
+    HOUSE: "Mukesh Ambani",
+    SECTOR: "Engineering - Construction",
+    perchange: -4.76,
+  },
+  {
+    FINCODE: 109073,
+    COMPNAME: "Hathway Bhawani Cabletel & Datacom Ltd.",
+    SCRIPCODE: 509073,
+    MCAP: 14.6691,
+    CLOSE_PRICE: 18.11,
+    SYMBOL: "0",
+    Status: "Active",
+    SNAME: "HathwayBhawani Cable",
+    scripcode1: 509073,
+    HOUSE: "Mukesh Ambani",
+    SECTOR: "TV Broadcasting & Software Production",
+    perchange: 0.44,
+  },
+];
